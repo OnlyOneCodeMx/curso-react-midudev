f279d304a9e254230af076e7e77eb4f76982533d
Miguel Angel Diaz <101842830+OnlyOneCodeMx@users.noreply.github.com>
2023-10-30 17:04:50 -0600

    update repo with third project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const App = () => {
+function App () {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0})
+
+  useEffect(() => {
+    console.log('effect ', { enabled })
+    
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log('handleMove', {clientX, clientY});
+      setPosition({ x: clientX, y: clientY  })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+    // En la consola chromium se puede ver quien y cuantas veces se suscribe 
+    // a algun elemento usando getEventListeners(window)
+    // Limpiar useEffect cuando se desactiva el componente
+    // o cuando cambia la dependencia antes de ejecutar el efecto nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#rgba(0, 0, 0, 0.5)',
+        border: '2px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50, 
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}/>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar': 'Activar'} seguir el puntero
+      </button>
+    </main>
   )
 }
 
